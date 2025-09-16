4ae1e6d43bb999ee6567f4a7e423f36e2add507b
yannam-hema <yannamhema30@gmail.com>
2025-09-16 19:26:54 +0530

    Socket Programming

--- src/SocketProgramming/ServerSimpleTramissionMode.java
+++ src/SocketProgramming/ServerSimpleTramissionMode.java
@@ -31,32 +31,4 @@ public static void main(String[] args) throws IOException {
 }
 }
 
-// CLIENT code should be in other work space to see the results
 
-//package Networking;
-//
-//import java.io.DataInputStream;
-//import java.io.DataOutputStream;
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.io.OutputStream;
-//import java.net.Socket;
-//import java.net.UnknownHostException;
-//import java.util.Scanner;
-//
-//public class ClientCode {
-//public static void main(String[] args) throws UnknownHostException, IOException {
-//	Socket csoc = new Socket("192.168.110.188",4000);
-//	OutputStream os = csoc.getOutputStream();
-//	DataOutputStream dos = new DataOutputStream(os);
-//	Scanner sc = new Scanner(System.in);
-//	System.out.println("Enter the message ");
-//	String msg = sc.nextLine();
-//	dos.writeUTF(msg);
-//	System.out.println("---------------");
-//	InputStream is = csoc.getInputStream();
-//	DataInputStream dis = new DataInputStream(is);
-//	msg=dis.readUTF();
-//	System.out.println("reply : "+msg);
-//}
-//}
